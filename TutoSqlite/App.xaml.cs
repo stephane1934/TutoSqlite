f24120f4f30d54db0f68851868abf8b0b9dfc56a
Stephane <stephaneher@hotmail.fr>
2020-02-10 06:50:20 +0100

    Ajoutez des fichiers projet.

---
+++ TutoSqlite/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TutoSqlite
+{
+    public partial class App : Application
+    {
+        public static string databasePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Contacts.db");
+    }
+}
---
+++ TutoSqlite/NewContactWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TutoSqlite.Classes;
+
+namespace TutoSqlite
+{
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneNumberTextBox.Text
+            };
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                conn.Insert(contact);
+            }
+
+            Close();
+        }
+
+    }
+}
